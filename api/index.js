bfe6941150d9bb16fe22508bd0135ce6b6f61001
HArish <121327487+harishcsx@users.noreply.github.com>
2024-10-10 12:58:17 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -24,6 +24,79 @@ app.get("/submit", (req, res) => {
             <title>Image Finder</title>
             <style>
                 /* Your CSS */
+                 @import url('https://fonts.googleapis.com/css2?family=Fira+Code:wght@300..700&display=swap');
+        
+        body {
+            height: 100vh;
+            margin: 0;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            font-family: 'Fira Code', serif;
+            background-color: black;
+            background-image: linear-gradient(to top, #30cfd0 0%, #330867 100%);
+            text-align: center;
+        }
+
+        .box {
+            background-color: rgb(213, 213, 213);
+            padding: 30px;
+            border-radius: 20px;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            flex-direction: column;
+            border: 0.01px solid rgb(60, 60, 60);
+            max-width: 90%;
+            width: 400px;
+        }
+
+        input {
+            all: unset;
+            width: 100%; /* Ensures the input takes the full width of the parent */
+            margin: 20px 0;
+            padding: 10px;
+            border-radius: 10px;
+            background-color: aliceblue;
+            border: 0.01px solid rgb(60, 60, 60);
+            text-align: center;
+            font-size: 1em;
+            box-sizing: border-box; /* Ensures padding and borders are included in the element's total width */
+        }
+
+        button {
+            all: unset;
+            background-image: linear-gradient(to top, #00c6fb 0%, #005bea 100%);
+            color: white;
+            padding: 10px 20px;
+            border-radius: 15px;
+            font-size: 1em;
+        }
+
+        button:hover {
+            cursor: pointer;
+            background-image: linear-gradient(to top, #a3bded 0%, #6991c7 100%);
+        }
+
+        @media (max-width: 600px) {
+            body {
+                padding: 20px;
+            }
+
+            .box {
+                padding: 20px;
+                width: 100%;
+            }
+
+            input {
+                font-size: 0.9em;
+            }
+
+            button {
+                font-size: 0.9em;
+                padding: 8px 16px;
+            }
+        }
             </style>
         </head>
         <body>
